3b6f62b66a5de88bc738fd48793ea3c0faaf0965
guilhermeaquino-fiserv <guilherme.aquino@fiserv.com>
2025-08-21 14:59:27 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,13 +4,23 @@ import streamlit as st
 # Título do app
 st.title("Multiplicador de X e Y")
 
-# Campos de entrada
-x = st.number_input("Digite o valor de X", value=0)
-y = st.number_input("Digite o valor de Y", value=0)
-debit = st.number_input("Digite o valor de debit", value=0.0)
-cred = st.number_input("Digite o valor de cred", value=0.0)
-parc6 = st.number_input("Digite o valor de parc6", value=0.0)
-parc12 = st.number_input("Digite o valor de parc12", value=0.0)
+# Layout em colunas
+col1, col2 = st.columns(2)
+
+
+x = st.number_input("CNPJ", value=0)
+y = st.number_input("CNAE", value=0)
+
+with col1:
+    debit = st.number_input("Digite o valor de debit", value=0.0)
+    cred = st.number_input("Digite o valor de cred", value=0.0)
+
+with col2:    
+    parc6 = st.number_input("Digite o valor de parc6", value=0.0)
+    parc12 = st.number_input("Digite o valor de parc12", value=0.0)
+    
+
+
 
 # Botão para calcular
 if st.button("Multiplicar"):
